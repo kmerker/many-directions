186e920f3101d05c2b4e4af43e64f2c56ebff40a
Michelle Noorali <michellemolu@gmail.com>
2016-03-10 19:16:25 +0000

    add yaml files and readinessProbe

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ build:
 	go build .
 
 image:
-	docker build -t mnoorali/many-directions:0.0.3 .
+	docker build -t mnoorali/many-directions:1.0.0 .
 
 push:
-	docker push mnoorali/many-directions:0.0.3
+	docker push mnoorali/many-directions:1.0.0
 
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	redis "gopkg.in/redis.v3"
 )
@@ -19,12 +20,15 @@ func main() {
 	http.HandleFunc("/index", indexHandler)
 	http.HandleFunc("/healthz", healthHandler)
 	http.HandleFunc("/play", playHandler)
+	http.HandleFunc("/ready", readyHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func start() {
 	// call service that gets and stores songs
 	topTracks := getSongs()
+	time.Sleep(60 * time.Second)
+	redisClient.Set("example song", "exampleid", 0)
 	for _, track := range topTracks.Tracks {
 		redisClient.Set(track.Title, track.Id, 0)
 	}
@@ -44,5 +48,23 @@ func playHandler(res http.ResponseWriter, req *http.Request) {
 	song, _ := redisClient.RandomKey().Result()
 	io.WriteString(res, "Play \""+song+"\" by One Direction!")
 	fmt.Printf("\nPlay %s by One Direction for us!\n", song)
+}
 
+func readyHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("\nA readiness check was performed at" + time.Now().String())
+	song, err := redisClient.RandomKey().Result()
+	if err != nil {
+		fmt.Println("Looks like there was a problem getting stuff from redis")
+		res.WriteHeader(http.StatusServiceUnavailable)
+		io.WriteString(res, "Problem with redis command.")
+	}
+	if song == "" {
+		fmt.Println("No songs in redis to find")
+		res.WriteHeader(http.StatusServiceUnavailable)
+		io.WriteString(res, "No songs!")
+	}
+
+	res.WriteHeader(http.StatusOK)
+	io.WriteString(res, "JAM ON!")
+	fmt.Println("JAM ON BRO")
 }
